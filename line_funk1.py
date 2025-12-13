3675693932c9082a3e79e0c75117376d4aa1a876
Tima <timofey120310@gmail.com>
2025-12-13 21:18:00 +0300

    chore:add division

--- line_funk1.py
+++ line_funk1.py
@@ -1,7 +1,15 @@
 def sum(a,b):
     return a + b
-print(sum(2, 1))
 
 def sum1(a,b):
     return a - b
-print(sum1(2, 1))
+
+result1 = sum(2, 1)
+result2 = sum1(2, 1)
+
+# Делим результаты
+division = result1 / result2
+
+print("Первый результат:", result1)
+print("Второй результат:", result2)
+print("Деление:", division)
