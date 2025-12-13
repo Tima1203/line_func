8a117f86de5f972340b1b88fa1f9c255d7c6c89d
Tima <timofey120310@gmail.com>
2025-12-13 21:48:56 +0300

    chore:the complete solution of the equation

--- line_funk1.py
+++ line_funk1.py
@@ -1,15 +1,42 @@
 def sum(a,b):
     return a + b
 
-def sum1(a,b):
+def min(a,b):
+    return a - b
+
+def min1(a,b,c):
+    return a*(b - c)
+
+def sum2(a,b,c):
+    return a*(b + c)
+
+def func(a,b):
+    return a - b
+
+def min2(a,b):
     return a - b
 
 result1 = sum(2, 1)
-result2 = sum1(2, 1)
+result2 = min(2, 1)
+result3 = min1(3, 2, 1)
+result4 = sum2(7, 2, 1)
+result6 = min2(1, 4)
 
-# Делим результаты
+z = 8
+
 division = result1 / result2
+division1 = result3 / result4
+result5 = division - division1
+division2 = z / result6
+result7 = result5 + division2
 
 print("Первый результат:", result1)
 print("Второй результат:", result2)
-print("Деление:", division)
+print("первая дробь:", division)
+print("третий результат:", result3)
+print("четвёртый результат:", result4)
+print("вторая дробь:", division1)
+print("разность 2 первых дробей:", result5)
+print("пятый результат:", result6)
+print("третяя дробь:", division2)
+print("ответ уравнения:", result7)
